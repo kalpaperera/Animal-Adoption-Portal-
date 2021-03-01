9da7bbcac66450fd7b21f03821ded1583239e8d3
Cosette <78508602+porrascosette@users.noreply.github.com>
2021-03-01 13:54:19 +0800

    Add files via upload

---
+++ AnimalService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalAdoption.Common.Logic
+{
+    public class AnimalService
+    {
+        public Animal[] ListAnimals => new Animal[] {
+            new Animal { Id = 1, Name = "OX", Age = 10, Description = "Strong, brave" },
+            new Animal { Id = 2, Name = "rabbit", Age = 1, Description = "soft, fluffy" },
+            new Animal { Id = 3, Name = "dog", Age = 3, Description = "loyal" },
+        };
+    }
+}
